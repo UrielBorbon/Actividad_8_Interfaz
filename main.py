67c1491925859f5afc08ce8214824ea4b4edd703
Uriel Borbon <alfredo.medina4934@alumnos.udg.mx>
2020-10-08 11:56:06 -0500

    Actividad 8 Interfaz De Usuario

---
+++ main.py
@@ -0,0 +1,9 @@
+from PySide2.QtWidgets import QPushButton, QApplication
+import sys
+from mainwindow import MainWindow
+# Aplicación de Qt
+app = QApplication()
+window = MainWindow()
+window.show()
+# Qt loop
+sys.exit(app.exec_())
